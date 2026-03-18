a482e20529f6ce8e005637b9f9875df8e17d8e87
muyuruhua <muyuruhua@gmail.com>
2026-03-18 19:10:11 +0800

    refine ablation result pos and run_ablation.sh

--- ChatAFL-master/mbfuzzer_artifact/mbfuzzer/helper_functions/network_operation.py
+++
@@ -1,5 +0,0 @@
-import socket
-
-def check_port(ip, port):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex((ip, port)) == 0
--- ChatAFL-master/mbfuzzer_artifact/mbfuzzer/type/VersionError.py
+++
@@ -1,4 +0,0 @@
-class VersionError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
